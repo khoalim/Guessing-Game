0cc3bf2efb50ffbb270929b47d5cd61471b1714f
Kim Hoalim <kthoalim@gmail.com>
2019-09-05 13:38:15 -0400

    Initial Commit

---
+++ GuessingGameDay3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GuessingGameDay3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please guess a number between 1 and 10");
+
+        }
+    }
+}
